807db9b7ede21631736fdb395a36638e3df63101
astinchoi <asciineo@gmail.com>
2016-02-01 18:12:37 +0900

    spike flask-socketio and change UI

--- spike/flask-socketio/sending_message.py
+++ spike/flask-socketio/sending_message.py
@@ -15,16 +15,28 @@ def handle_json(json):
 
 # @socketio.on('my event')
 # def handle_my_custom_event(json):
-#     emit('my response', json)
+#     emit('my response', str(json))
 
-@socketio.on('message')
-def handle_message(message):
-    send(message, namespace='/chat')
+
+# @socketio.on('message')
+# def handle_message(message):
+#     send(message, namespace='/chat')
+
+# @socketio.on('my event')
+# def handle_my_custom_event(json):
+#     emit('my response', json, namespace='/chat')
+
+# @socketio.on('my event')
+# def handle_my_custom_event(json):
+#     emit('my response', ('foo', 'bar', json), namespace='/chat')
+
+
+def ack():
+    print('message was received!')
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
-    print(str(json))
-    emit('my response', json, namespace='/chat')
+    emit('my response', str(json), callback=ack)
 
 
 if __name__ == '__main__':
